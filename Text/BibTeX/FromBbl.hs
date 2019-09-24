a3e86a44f82a760ae95a6a254eeae77790b1c85a
Justus Sagemüller <jsag@hvl.no>
2019-09-24 11:18:05 +0200

    Something that parses the first line of bbl.

--- Text/BibTeX/FromBbl.hs
+++ Text/BibTeX/FromBbl.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TypeFamilies          #-}
 
+module Text.BibTeX.FromBbl where
+
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
@@ -8,14 +10,18 @@ import qualified Text.BibTeX.Entry as BibTeX
 import Data.Void
 
 braced :: Parsec Void String String
-braced = do {char '{'; s <- many $ satisfy (\='}'); char '}'; return s}
+braced = do {char '{'; s <- many $ satisfy (/='}'); char '}'; return s}
 
 bblEntry :: Parsec Void String (BibTeX.T)
 bblEntry = do
-  identifier <- string "\\entry{" >> braced
+  identifier <- string "\\entry" >> braced
   entryType <- braced
   return $ BibTeX.Cons entryType identifier []
 
 main :: IO ()
 main = do
-  f <-
+  let fName = "web.bbl"
+  fConts <- readFile fName
+  let entry = runParser bblEntry fName fConts
+  print entry
+  return ()
